5115659453dbe853f56c028d5f620da75ec24d38
naahgabby1 <naahgabby1@gmail.com>
2025-03-24 17:30:24 +0000

    Update DbController.php

--- model/DbController.php
+++ model/DbController.php
@@ -5,7 +5,7 @@ class DbConnection
 protected function conn()
 {
 try {  
-@$conn = new PDO('mysql:host=localhost;dbname=db_aduanefieonboard','root','',array(
+@$conn = new PDO('mysql:host=localhost;dbname=db_aduanefieonboard','Gabby','gabbyinchrist',array(
 PDO::ATTR_PERSISTENT => TRUE));
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
@@ -18,11 +18,11 @@ die($e->getMessage());
 }
 
 
-protected function conx()
+protected function conx(
 {
 $servername ="localhost";
-$username ="root";
-$password ="";
+$username ="Gabby";
+$password ="gabbyinchrist";
 $dbname ="db_aduanefieonboard";
 $conx = mysqli_connect ($servername, $username, $password, $dbname);
 return $conx;
